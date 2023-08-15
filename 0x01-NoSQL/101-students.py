f689562db746a7667fae7741af3e072c2af290b4
sylvan452 <sylvan452@gmail.com>
2023-08-15 06:57:11 -0700

    Nosql advance tasks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+all students
+"""
+
+
+def top_students(mongo_collection):
+    """ students by their scores """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
